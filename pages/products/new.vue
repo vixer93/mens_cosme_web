93d296841392f3f698aef9ac89551d06bb5721a1
shin usuda <sasasa.24.blue@gmail.com>
2020-03-27 16:43:26 +0900

    Add preview function

--- pages/products/new.vue
+++ pages/products/new.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="new-product">
-  <form class="new-product-form">
-    <h1 class="new-product-title">商品追加</h1>
-    <input v-model="name" type="text" class="new-product-name" placeholder="商品名">
-    <input v-model="brand" type="text" class="new-product-email" placeholder="ブランド">
-    <input v-model="price" type="text" class="new-product-password" placeholder="価格">
-    <input @change="handleChangeFile" type="file" class="new-product-file" placeholder="価格" multiple="multiple">
-    <p class="new-product-error-message">{{ error }}</p>
-    <button @click.prevent="add" class="new-product-button">登録</button>
-  </form>
+    <form class="new-product-form">
+      <h1 class="new-product-title">商品追加</h1>
+      <div v-if="image_urls.length>0" class="new-product-preview">
+        <div v-for="image_url in image_urls">
+          <img :src="image_url" class="new-product-preview-image">
+        </div>
+      </div>
+      <label v-if="images.length < 4">
+        <div class="new-product-images">
+          <font-awesome-icon icon = "camera-retro" class="new-product-camera" />
+          <input @change="handleChangeFile" type="file" class="new-product-file" placeholder="価格" multiple="multiple">
+        </div>
+      </label>
+      <input v-model="name" type="text" class="new-product-name" placeholder="商品名">
+      <input v-model="brand" type="text" class="new-product-email" placeholder="ブランド">
+      <input v-model="price" type="text" class="new-product-password" placeholder="価格">
+      <p class="new-product-error-message">{{ error }}</p>
+      <button @click.prevent="add" class="new-product-button">登録</button>
+    </form>
   </div>
 </template>
 
@@ -22,14 +32,19 @@ export default {
       brand: "",
       price: null,
       images: [],
+      image_urls: [],
       error: "",
     }
   },
   methods: {
     handleChangeFile(event){
-      let images = this.images
+      let images     = this.images
+      let image_urls = this.image_urls
+
       images.push(event.target.files[0])
+      image_urls.push(URL.createObjectURL(event.target.files[0]))
       this.images = images
+      this.image_urls = image_urls
     },
 
     add(){
@@ -112,6 +127,40 @@ export default {
   .new-product-error-message {
     color: red;
     margin-bottom: 10px;
+  }
+
+  .new-product-images {
+    background-color: #f4f8fa;
+    border: dashed 3px #616161;
+    height: 60px;
+    width: 380px;
+    font-size: 18px;
+    margin: 0 auto 20px auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .new-product-camera {
+    font-size: 40px;
+    color: #616161;
+    line-height: 60px;
+  }
+
+  .new-product-file {
+    display: none;
+  }
+
+  .new-product-preview {
+    width: 380px;
+    height: 150px;
+    margin: 0 auto 20px auto;
+    display: flex;
+  }
+
+  .new-product-preview-image {
+    max-width: 100%;
+    max-height: 100%;
   }
 }
 
