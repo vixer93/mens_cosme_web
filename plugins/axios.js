40b405f155d27ff06d014dac1c9e74c17838acd4
shin usuda <sasasa.24.blue@gmail.com>
2020-04-14 22:54:19 +0900

    Fix

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,34 +3,3 @@ import axios from "axios";
 export default axios.create({
   baseURL: process.env.API_ENDPOINT
 })
-
-// export default ({$axios, store, app}) => {
-//   $axios.create({
-//     baseURL: process.env.API_ENDPOINT
-//   })
-
-  // axios.onRequest(config => {
-  //   const headers = store.user.auth
-  //   config.headers = headers
-  // })
-
-  // axios.onRespose(res => {
-  //   if (res.headers['access-token']) {
-  //     const authHeader = {
-  //       'access-token': res.headers['access-token'],
-  //       'client':       res.headers['client'],
-  //       'expire':       res.headers['expire'],
-  //       'uid':          res.headers['uid']
-  //     }
-  //     store.commit('user/setAuth', authHeader)
-
-  //     const session = app.$cookies.get('session')
-  //     if (session) {
-  //       app.$cookies.set('session',session, {
-  //         path: '/',
-  //         maxAge: 60 * 60 * 24 * 7
-  //       })
-  //     }
-  //   }
-  // })
-// }
